218f415525b57b7da3ff816c567ca22070239aa8
roshinidevanandi <roshinidevanandi@gmail.com>
2025-04-05 20:13:58 +0530

    yes

---
+++ python/datatypes/set.py
@@ -0,0 +1,82 @@
+# -----------------SET--------------
+
+# It is a collection of unique data
+# Set is muttable we can change the value of it
+# It is a unordered collection of data
+# It won't allow the duplicates
+
+
+# st={}
+# print(type(st))
+
+# detail=set()
+# print(detail)
+
+
+# new_set={1,2,8,9,7,6,4,3,5}
+# print(new_set)
+
+# add()
+# new_set.add(98)
+# print(new_set)
+
+# remove()
+# new_set.remove(9)
+# print(new_set)
+
+# pop()  ------------It removes the starting element
+# new_set.pop()
+# new_set.pop()
+# new_set.pop()
+# new_set.pop()
+# print(new_set)
+
+
+# clear()
+# new_set.clear()
+# print(new_set)
+
+# copy()
+# update=new_set.copy()
+# print(update)
+
+
+
+
+s1={2,8,6,9,4,5,3,49,89,96,99}
+s2={9,8,0,6,7,4,5,3,2}
+
+# union()
+# print(s1.union(s2))
+
+
+# intersection()
+# print(s1.intersection(s2))
+
+# difference()
+# print(s2.difference(s1))
+# print(s1.difference(s2))
+
+# symmetric_difference
+# print(s1.symmetric_difference(s2))
+
+
+# ---------------------frozenset------------------
+
+# It is immutable we can't change the values
+# We can't modify the set
+
+# fset1=frozenset({2,1,8,5})
+# fset2=frozenset({84,3,2,9,5,8,1})
+
+
+# print(type(fset1))
+# print(fset1)
+
+# print(fset1.issubset(fset2))
+# print(fset2.issubset(fset1))
+
+
+# print(fset1.issuperset(fset2))
+# print(fset2.issuperset(fset1))
+
