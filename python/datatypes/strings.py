668d77fd791c955d1bdfc38e8ad21426bbe91e36
roshinidevanandi <roshinidevanandi@gmail.com>
2025-04-06 22:20:33 +0530

    yes

--- python/datatypes/strings.py
+++ python/datatypes/strings.py
@@ -7,7 +7,8 @@
 
 
 
-data=" helloeveryONE234"
+# data="helloeveryONE234"
+
 # print(data)
 
 # data[0]="r"    -->TypeError: 'str' object does not support item assignment
@@ -64,5 +65,121 @@ data=" helloeveryONE234"
 # print(data.center(30,"*"))
 
 
-for i in range(len(data)):
-    print(data[i])
+# for i in range(len(data)):
+#     print(data[i])
+
+
+# frds=" "
+# isspace()
+# print(frds.isspace())
+
+
+# split()
+# data1="i am roshini from peddapalli"
+# new=data1.split()
+# print("".join(new))
+
+
+
+# ASCII ------------->>American Standard Code For Information Interchange
+
+# 0-9  ----->48-57
+# A-Z  ----->65-90
+# a-z  ----->97-122
+
+# ord
+# print(ord("&"))
+# print(ord("9"))
+# print(ord("*"))
+
+# chr
+# print(chr(69))
+# print(chr(98))
+# print(chr(42))
+
+
+# id  ------>To find the address of the variable
+# value=98
+# print(id(value))
+
+
+
+
+
+
+
+# ________________________PROBLEM SOLVING____________________
+
+# palindrome
+# 1.find the given string is palindrom or not palindrome
+
+# data3="racecars"
+# new_data=""
+# for i in range(len(data3)-1,-1,-1):
+#     new_data+=data3[i]   
+# if data3==new_data:
+#     print(data3,"is palindrome")
+# else:
+#     print(data3,"is not palindrome")
+
+
+
+
+# 2.find the most repeated letter in the given sentance
+
+# string="this is the python class"
+
+# most=""
+# count=0
+# for i in range(len(string)):
+#     if string.count(string[i])>count:
+#         most=string[i]
+#         count=string.count(string[i])
+
+# print(most)
+
+
+
+# anagram
+
+# 3.find the given two words are anagrams or not ANAGRAMS 
+
+# str1="silent"
+# str2="listen"
+
+# if len(str1)!=len(str2):
+#     print("not anagrams")
+# else:
+#     ana=True
+#     for i in range(len(str1)):
+#         if str1.count(str1[i])!=str2.count(str1[i]):
+#             ana=False
+#     if ana:
+#         print(str1,str2,"are anagrams")
+
+#     else:
+#         print("not anagrams")
+
+
+
+
+
+# ____________________________Slicing______________________
+
+str3="hello i am pupy"
+
+# print(str3[::])
+# print(str3[:5:])
+# print(str3[:5:2])
+# print(str3[5:])
+# print(str3[5:60])
+# print(str3[::-1]) ------>for reverse of the str
+
+
+# palindrome        
+
+# str="rar"
+# print(str==str[::-1])
+
+
+
