7a408210305cd1cad76864b8c164b698daae852e
roshinidevanandi <roshinidevanandi@gmail.com>
2025-04-07 22:36:50 +0530

    yes

---
+++ python/modules.py
@@ -0,0 +1,28 @@
+# _______________________MODULES___________________
+
+# In Python, a module is a file containing Python code, such as functions, classes, or 
+# variables, that can be imported and used in other programs.
+
+# 1.Built-in 
+# math, time, random, re, os
+
+# 2.custom/userdefined
+# .py
+
+# 3.third-party module
+# pip install numpy pandas matplotlib
+
+
+
+# ________MATH______
+
+import math
+
+# print(math)
+
+# print(math.ceil(3.1))
+# print(math.floor(4.9))
+# print(math.pow(5,3))
+# print(math.sqrt(625))
+# print(math.factorial(3))
+# print(math.pi)
