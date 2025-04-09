11c7f959b48034546eb48fc81d7717a997646c4f
roshinidevanandi <roshinidevanandi@gmail.com>
2025-04-09 22:19:39 +0530

    yes

---
+++ python/oops_concept/classes_objects.py
@@ -0,0 +1,66 @@
+# _________________CLASS_______________
+
+# Class is ablue print of an object
+# Class can be defined as a collcetion of objects
+# It is a logical entity
+# It is a model to create an object
+# Class is a template to create an object
+
+# Syntax
+
+# class class_name:
+    #variable declaration (instance variable)
+    # method or function declaration
+
+# ____________OBJECT________
+
+# Object is a instance of a class
+# It is an entity that existing in the real world
+# It is a physical entity
+
+# Syntax
+
+# object_name=class_name()
+
+
+# Examples:
+
+# class         object
+# car           BMW, Tata,Skeda.......
+# mobile        Redmi,Vivo,oppo.....
+# fruits        Banana,Apple,Orange....
+# dress         tops,chudidhars,jeans.....
+
+
+
+
+
+# Types of variables
+# Public
+# Private
+
+# Public Variables
+# We can access it in anywhere
+# example:  x=90
+
+# Private Variables
+# We can access within a class only.if you use outside the class you'll get an error
+# example:  _ _x=90 (we have to declare it with two underscores)
+
+
+
+# Example 1:
+
+# class Add:
+#     x=100
+#     y=200
+
+#     def sum(self):
+#         print(f"Addition of two numbers is {self.x+self.y}")
+    
+# obj1=Add()
+# obj1.sum()
+
+
+# Example 2:
+     
