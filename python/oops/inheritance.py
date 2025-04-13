25740a9f801bc9c2819380aca38050144654d4fa
roshinidevanandi <roshinidevanandi@gmail.com>
2025-04-13 19:54:08 +0530

    yes

---
+++ python/oops/inheritance.py
@@ -0,0 +1,14 @@
+# _____________________INHERITANCE________________
+
+# The process of creating a new class from already existing class is called as inheritance
+# with the help of this we can extend the code
+# code will be reusable
+# It is used to save the time
+
+# Types of interitance:
+# 1.Single inheritance
+# 2.Multi-level inheritance
+# 3.Multiple inheritance
+# 4.Hierarical inheritance
+# 5.Hybrid inheritance
+
